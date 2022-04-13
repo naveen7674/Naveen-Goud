1f1e324bb3ef60b4dbd67d88f1f05d72d1a50321
naveen7674 <102161860+naveen7674@users.noreply.github.com>
2022-04-13 20:41:01 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,8 @@ use App\Http\Controllers;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('home', function () {
+    header("Location: index.html");
+});
 Route::view('/contact', 'contact');
 Route::view('/about', 'about');
