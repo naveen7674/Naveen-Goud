5bde40d88281738f1e04eafcc988053a71b14502
naveen7674 <102161860+naveen7674@users.noreply.github.com>
2022-06-07 18:59:05 +0100

    init

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use App\Http\Controllers;
 // Route::get('/', function () {
 //     return view('index');
 // });
-// Route::get('about', function () {
-//     return view('about');
-// });
+Route::get('about', function () {
+    return view('about');
+});
 
 
